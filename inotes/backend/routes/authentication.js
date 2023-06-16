9d021f7f13a2ab9b61b6119b1d55dc43a9e66813
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-16 18:08:35 +0545

    Creating ThunderClient collections to manage request

--- inotes/backend/routes/authentication.js
+++ inotes/backend/routes/authentication.js
@@ -3,21 +3,33 @@ const router = express.Router();
 const User= require('../models/User');
 const { query, validationResult, body } = require('express-validator');
 
-// create a user using: POST "api/authentication/". Doesn't require Authentication
-router.post('/', [
+// create a User using: POST "api/authentication/createuser". No login required
+router.post('/createuser', [
     body('email','Enter valid email').isEmail(),
     body('name','Enter valid name').isLength({ min: 3}),
     body('password').isLength({ min: 5}),
-],(req, res)=>{
+],async (req, res)=>{
+    // If there are errors, return and the errors
     const result = validationResult(req);
     if (!result.isEmpty()) {
         return res.send({ errors: result.array() });
     } 
-    User.create({
+    // Check whether the user with this email exists already
+    try{
+    let user =await User.findOne({email: req.body.email})
+    if(user){
+        return res.send({ error: "User with this email already exists"});
+    }
+    user = await User.create({
         name: req.body.name,
         password: req.body.password,
         email: req.body.email,
-    }).then(user => res.json(user));
+    })
+    res.json(user)
+} catch(error){
+    console.error(error.message);
+    res.status(500).send("some error occured");
+}
 })
 
 module.exports=router
