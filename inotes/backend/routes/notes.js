e378b922c9d7406d543b6a4fd9d891513dd8bc2a
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-21 17:56:07 +0545

    creating end points for creating a note and fetching all the notes

--- inotes/backend/routes/notes.js
+++ inotes/backend/routes/notes.js
@@ -1,8 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
+const { query, validationResult, body } = require("express-validator");
 
-router.get('/', (req, res)=>{
-    res.json([])
-})
+// Route 1: Retrieve all the notes using: GET "api/notes/fetchNotes". Login required
+router.get("/fetchNotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("some error occured");
+  }
+});
 
-module.exports=router
+// Route 2: Adding a new note using: POST "api/notes/addNote". Login required
+router.post(
+  "/addNote",
+  fetchuser,
+  [
+    body("title", "Enter valid title").isLength({ min: 3 }),
+    body("description", "Enter description with atleast 5 characters").isLength(
+      { min: 5 }
+    ),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      // If there are errors, return the errors
+      const result = validationResult(req);
+      if (!result.isEmpty()) {
+        return res.send({ errors: result.array() });
+      }
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      })
+      const savedNote = await note.save();
+      res.json(savedNote);
+      
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occured");
+    }
+  }
+);
+
+module.exports = router;
