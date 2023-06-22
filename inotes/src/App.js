7940f871598fd9e707b51bdaf8dc3348af925dea
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-22 14:56:04 +0545

    using tailwind css for the navbar and  using routing throung different pages from navbar

--- inotes/src/App.js
+++ inotes/src/App.js
@@ -1,8 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-<h1>Helloooo</h1>
+    <>
+    <Router>
+    <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
