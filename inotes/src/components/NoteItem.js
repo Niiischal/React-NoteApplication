8369f3ec4d4c66f0123f84bf84f73e09fe8b36d4
Niiischal <nischal.khatiwada745@gmail.com>
2023-06-26 14:29:54 +0545

    making some changes

--- inotes/src/components/NoteItem.js
+++ inotes/src/components/NoteItem.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import NoteContext from '../context/notes/NoteContext';
 
 const NoteItem = (props) => {
+  const context = useContext(NoteContext)
+  const {deleteNote}= context
     const {note}=props;
   return (
     <div className='col-md-3'>
@@ -9,7 +12,7 @@ const NoteItem = (props) => {
     <h1 className="card-title"> {note.title}:</h1>
     <p className="card-text">{note.description}</p>
     <div className=" my-2 mb-0 d-flex justify-content-around">
-    <i className="fa-solid fa-trash mx-5" style={{color: "#ec4b5b"}}></i>
+    <i className="fa-solid fa-trash mx-5" style={{color: "#ec4b5b"}} onClick={()=>{deleteNote(note._id)}}></i>
     <i className="fa-solid fa-pen-to-square mx-5" style={{color: "#3726ba"}}></i>
     </div>
   </div>
